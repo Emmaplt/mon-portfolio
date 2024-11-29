0a7eb7693a80ab218560a6f58e6cecb74bb56a47
Emma Plaut <emma.plaut@hotmail.fr>
2024-11-29 13:54:03 +0100

    Mise en place du footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import './_footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import logo from "../../assets/images/logo-ymeria.png";
+import './_footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-      <img src={logo} className="App-logo-footer" alt="logo"/>
+        <img src={logo} className="App-logo-footer" alt="logo" />
         <p>© 2024 Emma Plaut</p>
         <div className="footer-socials">
-          <a href="https://github.com/Emmaplt" target="_blank" rel="noopener noreferrer">
-            <img src="/assets/icons/github.svg" alt="GitHub" />
+          <a
+            href="https://linkedin.com/in/emma-plaut-bb0040167"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-linkedin"
+          >
+            <div className="icon-circle icon-linkedin-circle">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </div>
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="/assets/icons/linkedin.svg" alt="LinkedIn" />
+          <a
+            href="https://github.com/Emmaplt"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-github"
+          >
+            <div className="icon-circle icon-github-circle">
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
           </a>
         </div>
       </div>
