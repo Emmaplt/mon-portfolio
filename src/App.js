f4505e060decd43e2146c3c07448c0a7676a8994
Emma Plaut <emma.plaut@hotmail.fr>
2024-12-06 14:45:01 +0100

    Fin du responsive tablette et telephone page d'accueil

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
                 <ProjectsCard />
                 <Services />
                 <Skills />
-                {/* <Contact /> */}
+                <Contact />
                 <Footer />
               </>
             }
